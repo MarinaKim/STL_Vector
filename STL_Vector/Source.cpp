90017f94fdcd0110a977fb3d8ba82d0b1254d893
Марина <marikaa@mail.ru>
2018-06-12 00:51:02 +0600

    -

--- STL_Vector/Source.cpp
+++ STL_Vector/Source.cpp
@@ -7,56 +7,12 @@
 Использовать контейнерный класс vector.
 */
 
-/* iterator: методы: 
-begin-end
-rbegin-rend в случае если нам необходимо посмотреть с конца
 
- end указывает на конец области, а не на последний элемент;
-*/
 #include "Library.h"
 #include"RailwayStation.h"
 
 int main() {
 	setlocale(LC_ALL, "Russian");
-	int n;
-	cout << "Enter task num:";
-	cin >> n;
-	switch (n) {
-		//example
-	case 1: {
-		vector<int> v;
-		v.push_back(5);
-		v.push_back(4);
-		v.push_back(19);
-		v.push_back(29);
-		v.push_back(9);
-		v.push_back(18);
-
-		//удаление
-		v.erase(v.begin());
-		for (int i = 0;i < 6/*v.size()*/;i++)
-			cout << v[i] << endl;
-		cout << "--------------------------------------------------" << endl;
-		//
-		cout << *(v.end() - 2) << endl;
-		cout << "--------------------------------------------------" << endl;
-
-		//используем внешнюю сортировку из бибилиотеки алгоритм
-		sort(v.begin(), v.end());
-		for (int i = 0;i <6/* v.size()*/;i++)
-			cout << v[i] << endl;
-		cout << "--------------------------------------------------" << endl;
-		// reverse-перевернуть
-		reverse(v.begin(), v.end());
-		for (int i = 0;i < 6/* v.size()*/;i++)
-			cout << v[i] << endl;
-		cout << "--------------------------------------------------" << endl;
-
-	}break;
-
-		//STL_Vector
-
-	case 2: {
 		/*vector<Train> t;*/
 		Railways t;
 		Train tmp;
@@ -79,10 +35,7 @@ int main() {
 		cout << "-----------------------------------------------------------------------------" <<endl;
 		cout << "Вывод информации о запрашиваемом поезде:" << endl;
 		t.print(h);
-	}break;
-
-	}
-
+	
 	system("pause");
 	return 0;
 }
